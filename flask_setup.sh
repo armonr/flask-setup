54dc54b7d258e993b306b84d173b79ca50377ae2
Armon <armonr@users.noreply.github.com>
2014-06-09 09:26:11 -0700

    Create flask_setup.sh

---
+++ flask_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# This must be run as root
+# provide full path to the directory for setting up flask
+# Once successfully run, activate virtualenv:
+# > source $1/venv/bin/activate
+# Now you can run flask powered python scripts
+# to deactivate, just run:
+# > deactivate
+
+# Localbox Mac OS X:
+
+if [ -z "$1" ]
+then
+	echo "Usage: $0 /fullpath/to/dir/to/install/flask"
+	exit -1
+fi
+
+# Is pip installed?
+command -v pip || { echo "\nInstalling pip ...\n"; easy_install pip; }
+
+# virtualenv ?
+command -v virtualenv || { echo "\nInstalling virtualenv ...\n"; pip install virtualenv; }
+
+# install flask
+mkdir -p "$1"
+cd "$1"
+virtualenv venv
+pip install Flask
+source venv/bin/activate
+
+echo "\nSuccess! You now have a flask installed in a virtualenv under $1/venv"
